44590160105b145cb27bdb47d5fdb7822970ce9e
DmitryGk <gulyackevichdmitry@gmail.com>
2022-04-11 14:00:23 +0300

    complete: Todo component, add: useState to App.tsx,

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { TodoList } from './TodoList';
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export type FilterValueType = 'all' | 'active' | 'completed'
 
 function App() {
+
+
+    let [tasks, setTasks] = useState([
+        { id: 1, title: 'dkjhfjkhd', isDone: false },
+        { id: 2, title: 'dkjhfjkhd', isDone: true },
+        { id: 3, title: 'fdf', isDone: false }
+    ])
+
+    let [filter, setFilter] = useState<FilterValueType>('all')
+    let tasksForTodoList = tasks
+    if(filter === 'active'){
+        tasksForTodoList = tasks.filter(t => t.isDone === false)
+    }
+    if(filter === 'completed') {
+        tasksForTodoList = tasks.filter(t => t.isDone === true)
+    }
+
+   
+    function removeTask(id: number) {
+        let filteredTasks = tasks.filter(t => t.id != id)
+        setTasks(filteredTasks)
+    }
+
+    function changeFilter(value: FilterValueType) {
+        setFilter(value)
+    }
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList
+                title={'What to do'}
+                tasks={tasksForTodoList}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+            />
         </div>
     );
 }
+
+
 
 export default App;
