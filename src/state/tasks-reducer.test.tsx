55df508fcd980208abc2dabe4164b7973215c7e7
DmitryGk <gulyakevichdmitry@gmail.com>
2022-04-14 23:54:30 +0300

    add: 2 actions to tasks reducer

--- src/state/tasks-reducer.test.tsx
+++ src/state/tasks-reducer.test.tsx
@@ -1,5 +1,5 @@
 import { TasksStateType } from "../App"
-import { tasksReducer } from "./tasks-reducer"
+import { addTaskAC, changeTaskStatusAC, removeTaskAC, tasksReducer } from "./tasks-reducer"
 
 
 test('correct task should be deleted from correct array', () => {
@@ -33,4 +33,64 @@ test('correct task should be deleted from correct array', () => {
         ]
      });
      
+})
+
+test('correct task should be added to correct array', () => {
+
+    const startState: TasksStateType = {
+        'todoListId1':[
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
+        ],
+        'todoListId2':[
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false}
+        ]
+    }
+
+    const action = addTaskAC('juice', 'todoListId2')
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState['todoListId1'].length).toBe(3)
+    expect(endState['todoListId2'].length).toBe(4)
+    expect(endState['todoListId2'][0].id).toBeDefined()
+    expect(endState['todoListId2'][0].title).toBe('juice')
+    expect(endState['todoListId2'][0].isDone).toBe(false)
+
+
+})
+
+test('status of specified task should be changed', () => {
+
+    const startState: TasksStateType = {
+        'todoListId1':[
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
+        ],
+        'todoListId2':[
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false}
+        ]
+    }
+
+    const action = changeTaskStatusAC('2', false, 'todoListId2')
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState['todoListId2'].length).toBe(3)
+    expect(endState['todoListId2'][1].isDone).toBe(false)
+    expect(endState['todoListId2'][0].isDone).toBe(false)
+    expect(endState['todoListId2'][1].title).toBe('milk')
+    expect(endState['todoListId1'][1].isDone).toBe(true)
+
+
+})
+
+test('title of current task should be changed', () => {
+    
 })
--- src/state/tasks-reducer.tsx
+++ src/state/tasks-reducer.tsx
@@ -1,23 +1,69 @@
-import { TasksStateType } from "../App";
+import { v1 } from "uuid";
+import { TasksStateType, TaskType } from "../App";
 
-export type removeTaskActionCreatorType = {
+export type RemoveTaskActionType = {
     type: 'REMOVE-TASK'
-    id: string
+    taskId: string
     todoListId: string
 }
-
-type ActionsType = 
-    removeTaskActionCreatorType
+export type AddTaskActionType = {
+    type: 'ADD-TASK'
+    taskTitle: string
+    todoListId: string
+}
+export type ChangeTaskStatusActionType = {
+    type: 'CHANGE-TASK-STATUS'
+    taskId: string
+    isDone: boolean
+    todoListId: string
+}
+type ActionsType =
+    RemoveTaskActionType |
+    AddTaskActionType |
+    ChangeTaskStatusActionType
 
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
         case 'REMOVE-TASK':
-            let todoListTasks = tasks[action.todoListId]       
-               return state
-        case '':
-            return state
-            default :
-            throw new Error ("I dom't understand this action type") 
+            let todoListTasks = state[action.todoListId]
+            let filteredTasks = todoListTasks.filter(t => t.id !== action.taskId)
+
+            return {
+                ...state,
+                [action.todoListId]: filteredTasks
+            }
+        case 'ADD-TASK':
+            return {
+                ...state,
+                [action.todoListId]: [
+                    { id: v1(), title: action.taskTitle, isDone: false },
+                    ...state[action.todoListId]
+                ]
+
+            }
+            case 'CHANGE-TASK-STATUS' :
+                let stateCopy = {...state}
+                let currentTodo = stateCopy[action.todoListId]
+                let currentTask = currentTodo.find( t => t.id === action.taskId)
+                if(currentTask){
+                    currentTask.isDone = action.isDone
+                }
+                return {
+                    ...state,
+                }
+
+        default:
+            throw new Error("I dom't understand this action type")
     }
+}
+
+export const removeTaskAC = (taskId: string, todoListId: string):RemoveTaskActionType => {
+    return { type: 'REMOVE-TASK', taskId, todoListId }
+}
+export const addTaskAC = (taskTitle: string, todoListId: string): AddTaskActionType => {
+    return { type: "ADD-TASK", taskTitle, todoListId }
+}
+export const changeTaskStatusAC = ( taskId: string, isDone: boolean, todoListId: string): ChangeTaskStatusActionType => {
+    return { type: 'CHANGE-TASK-STATUS', taskId, isDone, todoListId}
 }
