dc1a6a9023a65c23459ad6f161bfa988bb77e0e6
DmitryGk <gulyakevichdmitry@gmail.com>
2022-04-17 15:01:22 +0300

    add: Task.stories.tsx

--- src/Task.stories.tsx
+++ src/Task.stories.tsx
@@ -10,4 +10,24 @@ const changeTaskStatusCallback = action('Status changed inside Task')
 const changeTaskTitleCallback = action('Title changed inside Task')
 const removeTaskCallBack = action('Remove Button inside Task clicked')
 
-const Template: Story<> 
+const Template: Story<TaskPropsType> = (args) => <Task 
+{...args}/>
+const baseArgs = {
+    changeTaskStatus: changeTaskStatusCallback,
+    changeTaskTitle: changeTaskTitleCallback,
+    removeTask: removeTaskCallBack
+} 
+
+export const TaskIsDoneExample = Template.bind({});
+TaskIsDoneExample.args = {
+    ...baseArgs,
+    task: {id: '1', isDone: true, title: 'JS'},
+    todoListId: 'todoListId1'
+}
+
+export const TaskIsNotDoneExample = Template.bind({});
+TaskIsNotDoneExample.args = {
+    ...baseArgs,
+    task: {id: '1', isDone: false, title: 'JS'},
+    todoListId: 'todoListId1'
+}
